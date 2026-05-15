6a1eae9ee5d90082d2a41aeb7ee681285eefb8a6
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-15 20:52:29 +0700

    Update video scaling parameters in video.go

--- video.go
+++ video.go
@@ -20,7 +20,7 @@ func CreateVideo() error {
 
   "-vf",
 
-  "scale=720:1280:force_original_aspect_ratio=increase,crop=720:1280,zoompan=z='min(zoom+0.0015,1.5)':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=125",
+  "scale=900:1600:force_original_aspect_ratio=increase,crop=720:1280,zoompan=z='min(zoom+0.0015,1.5)':d=125:s=720x1280",
 
   "-c:v", "libx264",
 
