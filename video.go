f780a2d25870d2ccade7065588abf197d22d3c9f
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-15 21:00:07 +0700

    Update video.go

--- video.go
+++ video.go
@@ -20,7 +20,7 @@ func CreateVideo() error {
 
   "-vf",
 
-  "scale=900:1600:force_original_aspect_ratio=increase,crop=720:1280,zoompan=z='min(zoom+0.0015,1.5)':d=125:s=720x1280",
+  "scale=900:1600:force_original_aspect_ratio=increase,crop=720:1280,zoompan=z='min(zoom+0.0015,1.5)':d=250:s=720x1280",
 
   "-c:v", "libx264",
 
