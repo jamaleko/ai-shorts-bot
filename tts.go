75fc05d3796c40ba76cb15761861058cf6c5e338
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-15 17:51:49 +0700

    Update tts.go

--- tts.go
+++ tts.go
@@ -1,45 +1,26 @@
 package main
 
 import (
- "fmt"
- "io"
- "net/http"
- "net/url"
  "os"
+
+ htgotts "github.com/hegedustibor/htgo-tts"
 )
 
 func GenerateTTS(text string) error {
 
- ttsURL :=
-  "https://translate.google.com/translate_tts" +
-   "?ie=UTF-8" +
-   "&client=tw-ob" +
-   "&tl=id" +
-   "&q=" + url.QueryEscape(text)
-
- resp, err := http.Get(ttsURL)
-
- if err != nil {
-  return err
+ speech := htgotts.Speech{
+  Folder:   ".",
+  Language: "en",
  }
 
- defer resp.Body.Close()
+ speech.Speak(
+  text,
+  "voice",
+ )
 
- file, err := os.Create("voice.mp3")
+ _, err := os.Stat(
+  "voice.mp3",
+ )
 
- if err != nil {
-  return err
- }
-
- defer file.Close()
-
- _, err = io.Copy(file, resp.Body)
-
- if err != nil {
-  return err
- }
-
- fmt.Println("VOICE CREATED")
-
- return nil
+ return err
 }
