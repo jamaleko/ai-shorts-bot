cf841e47af824adea5e7be7922a95ce860f0a168
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-15 15:00:49 +0700

    Update rss.go

--- rss.go
+++ rss.go
@@ -1,35 +1,119 @@
 package main
 
 import (
+ "encoding/xml"
  "fmt"
+ "math/rand"
+ "net/http"
+
  "github.com/mmcdole/gofeed"
 )
 
 type FeedItem struct {
- Title string
- Link  string
+ Title       string
+ Link        string
+ Description string
+}
+
+type SitemapURL struct {
+ Loc string xml:"loc"
+}
+
+type Sitemap struct {
+ URLs []SitemapURL xml:"url"
 }
 
 func GetNews() (*FeedItem, error) {
 
- parser := gofeed.NewParser()
+ var allItems []FeedItem
 
- feed, err := parser.ParseURL(
-  "https://www.space.com/feeds.xml",
+ // ====================
+ // RSS FEEDS
+ // ====================
+
+ rssFeeds := []string{
+
+  "https://inet.detik.com/rss",
+
+  //"https://www.nasa.gov/news-release/feed/",
+ }
+
+ for _, rssURL := range rssFeeds {
+
+  parser := gofeed.NewParser()
+
+  feed, err := parser.ParseURL(rssURL)
+
+  if err != nil {
+
+   println("RSS ERROR:", rssURL)
+
+   continue
+  }
+
+  for _, item := range feed.Items {
+
+   allItems = append(
+    allItems,
+    FeedItem{
+     Title:       item.Title,
+     Link:        item.Link,
+     Description: item.Description,
+    },
+   )
+  }
+ }
+
+ // ====================
+ // SITEMAP KOMPAS
+ // ====================
+
+ sitemapURL :=
+  "https://www.kompas.com/sitemap-news-sains.xml"
+
+ resp, err := http.Get(sitemapURL)
+
+ if err == nil {
+
+  defer resp.Body.Close()
+
+  var sitemap Sitemap
+
+  xml.NewDecoder(resp.Body).
+   Decode(&sitemap)
+
+  for _, url := range sitemap.URLs {
+
+   allItems = append(
+    allItems,
+    FeedItem{
+     Title: "Artikel Kompas Sains",
+     Link:  url.Loc,
+    },
+   )
+  }
+ }
+
+ // ====================
+ // EMPTY CHECK
+ // ====================
+
+ if len(allItems) == 0 {
+
+  return nil, fmt.Errorf(
+   "semua feed kosong",
+  )
+ }
+
+ // ====================
+ // RANDOM ARTICLE
+ // ====================
+
+ randIndex := rand.Intn(
+  len(allItems),
  )
 
- if err != nil {
-  return nil, err
- }
+ item := allItems[randIndex]
 
- if len(feed.Items) == 0 {
-  return nil, fmt.Errorf("feed kosong")
- }
-
- item := feed.Items[0]
-
- return &FeedItem{
-  Title: item.Title,
-  Link:  item.Link,
- }, nil
+ return &item, nil
 }
