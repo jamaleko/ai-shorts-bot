4e40ba0e00017bc12341a1fda1b9b311a7bada3c
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-15 22:41:38 +0700

    Refactor CreateVideo function to use exec.Command

--- video.go
+++ video.go
@@ -2,7 +2,6 @@ package main
 
 import (
  "fmt"
- "os"
  "os/exec"
 )
 
@@ -15,88 +14,21 @@ func CreateVideo() error {
   fmt.Sprintf("%.2f", duration),
  )
 
- files, err := os.ReadDir(
-  "images",
- )
+ cmd := exec.Command(
 
- if err != nil {
-  return err
- }
+  "ffmpeg",
 
- imageCount := len(files)
-
- if imageCount == 0 {
-  return nil
- }
-
- secondsPerImage :=
-  duration / float64(imageCount)
-
- inputs := []string{
   "-y",
- }
 
- filter := ""
+  "-framerate", "1.0/15.0",
 
- for i := 1; i <= imageCount; i++ {
-
-  inputs = append(
-   inputs,
-
-   "-loop", "1",
-
-   "-t",
-   fmt.Sprintf(
-    "%.2f",
-    secondsPerImage,
-   ),
-
-   "-i",
-   fmt.Sprintf(
-    "images/%d.jpg",
-    i,
-   ),
-  )
-
-  filter += fmt.Sprintf(
-
-   "[%d:v]scale=900:1600:force_original_aspect_ratio=increase,crop=720:1280,zoompan=z='min(zoom+0.0015,1.5)':d=125:s=720x1280[v%d];",
-
-   i-1,
-   i-1,
-  )
- }
-
- filterConcat := ""
-
- for i := 0; i < imageCount; i++ {
-
-  filterConcat += fmt.Sprintf(
-   "[v%d]",
-   i,
-  )
- }
-
- filter += fmt.Sprintf(
-  "%sconcat=n=%d:v=1:a=0[outv]",
-  filterConcat,
-  imageCount,
- )
-
- inputs = append(
-  inputs,
+  "-i", "images/%d.jpg",
 
   "-i", "voice.mp3",
 
-  "-filter_complex", filter,
+  "-vf",
 
-  "-map", "[outv]",
-
-  "-map",
-  fmt.Sprintf(
-   "%d:a",
-   imageCount,
-  ),
+  "scale=900:1600:force_original_aspect_ratio=increase,crop=720:1280,zoompan=z='min(zoom+0.0008,1.3)':d=375:s=720x1280",
 
   "-c:v", "libx264",
 
@@ -104,14 +36,13 @@ func CreateVideo() error {
 
   "-c:a", "aac",
 
-  "-shortest",
+  "-t",
+  fmt.Sprintf(
+   "%.0f",
+   duration,
+  ),
 
   "video.mp4",
- )
-
- cmd := exec.Command(
-  "ffmpeg",
-  inputs...,
  )
 
  return cmd.Run()
