c006e910357e48e8598d52d5d2fe05f1cce9dfe1
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-15 18:35:23 +0700

    Update ai.go

--- ai.go
+++ ai.go
@@ -3,13 +3,34 @@ package main
 import (
  "fmt"
  "regexp"
+ "strings"
  )
-func CleanHTML(text string) string {
- re := regexp.MustCompile(`<[^>]*>`)
- return re.ReplaceAllString(
+func CleanHTML(
+ text string,
+) string {
+
+ re := regexp.MustCompile(
+  <[^>]*>,
+ )
+
+ clean := re.ReplaceAllString(
   text,
   "",
-  )
+ )
+
+ clean = strings.ReplaceAll(
+  clean,
+  "&#160;",
+  " ",
+ )
+
+ clean = strings.ReplaceAll(
+  clean,
+  "&#8230;",
+  "",
+ )
+
+ return clean
 }
 func GenerateShortScript(title string, description string) string {
 cleanDesc := CleanHTML(
