8526ec9ca38e677a274bbec7c8a732d42cf2f051
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-15 22:31:47 +0700

    Update video.go

--- video.go
+++ video.go
@@ -29,30 +29,74 @@ func CreateVideo() error {
   return nil
  }
 
- secondsPerImage := duration / float64(imageCount)
+ secondsPerImage :=
+  duration / float64(imageCount)
 
- frames := int(secondsPerImage * 25)
+ inputs := []string{
+  "-y",
+ }
 
- filter := fmt.Sprintf(
+ filter := ""
 
-  "scale=900:1600:force_original_aspect_ratio=increase,crop=720:1280,zoompan=z='min(zoom+0.0015,1.5)':d=%d:s=720x1280",
+ for i := 1; i <= imageCount; i++ {
 
-  frames,
+  inputs = append(
+   inputs,
+
+   "-loop", "1",
+
+   "-t",
+   fmt.Sprintf(
+    "%.2f",
+    secondsPerImage,
+   ),
+
+   "-i",
+   fmt.Sprintf(
+    "images/%d.jpg",
+    i,
+   ),
+  )
+
+  filter += fmt.Sprintf(
+
+   "[%d:v]scale=900:1600:force_original_aspect_ratio=increase,crop=720:1280,zoompan=z='min(zoom+0.0015,1.5)':d=125:s=720x1280[v%d];",
+
+   i-1,
+   i-1,
+  )
+ }
+
+ filterConcat := ""
+
+ for i := 0; i < imageCount; i++ {
+
+  filterConcat += fmt.Sprintf(
+   "[v%d]",
+   i,
+  )
+ }
+
+ filter += fmt.Sprintf(
+  "%sconcat=n=%d:v=1:a=0[outv]",
+  filterConcat,
+  imageCount,
  )
 
- cmd := exec.Command(
-
-  "ffmpeg",
-
-  "-y",
-
-  "-framerate", "1",
-
-  "-i", "images/%d.jpg",
+ inputs = append(
+  inputs,
 
   "-i", "voice.mp3",
 
-  "-vf", filter,
+  "-filter_complex", filter,
+
+  "-map", "[outv]",
+
+  "-map",
+  fmt.Sprintf(
+   "%d:a",
+   imageCount,
+  ),
 
   "-c:v", "libx264",
 
@@ -63,6 +107,11 @@ func CreateVideo() error {
   "-shortest",
 
   "video.mp4",
+ )
+
+ cmd := exec.Command(
+  "ffmpeg",
+  inputs...,
  )
 
  return cmd.Run()
