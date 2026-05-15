8461390683f8e13f188840dcd8bfa80d25fd6835
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-15 16:15:08 +0700

    Add CreateVideo function to generate video from files

---
+++ video.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+ "os/exec"
+)
+
+func CreateVideo() error {
+
+ cmd := exec.Command(
+
+  "ffmpeg",
+
+  "-loop", "1",
+
+  "-i", "thumbnail.jpg",
+
+  "-i", "voice.mp3",
+
+  "-c:v", "libx264",
+
+  "-tune", "stillimage",
+
+  "-c:a", "aac",
+
+  "-b:a", "192k",
+
+  "-pix_fmt", "yuv420p",
+
+  "-shortest",
+
+  "video.mp4",
+ )
+
+ return cmd.Run()
+}
